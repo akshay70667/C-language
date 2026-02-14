3de5ed62cb25af15650b6c8e5e371f4b1e3193c9
akshay70667 <akshay70667yd@gmail.com>
2026-02-14 14:11:54 +0530

    code updated

--- ppt.c
+++ ppt.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
-void square(int* n);
-int main() {
-    //pointer to pointer
-    int num = 10;      // Normal variable
-    int *ptr = &num;  
-    int **pptr = &ptr;
-    
-    printf("Value of num: %d\n", num);
-    printf("Value of **ptr: %d\n", **pptr);
-    printf("Value of *ptr: %d\n", *ptr);
-    square(&num);
+
+// Pointer to Pointer demonstration
+
+int main()
+{
+    int a;
+    int b;
+
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    int *ptrA = &a;         // pointer to a
+    int *ptrB = &b;         // pointer to b
+    int **ptrToPtrA = &ptrA;  // pointer to pointer to a
+
+    printf("\n--- VALUES ---\n");
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", b);
+
+    printf("Value using ptrA (*ptrA): %d\n", *ptrA);
+    printf("Value using ptrB (*ptrB): %d\n", *ptrB);
+
+    printf("Value using ptrToPtrA (**ptrToPtrA): %d\n", **ptrToPtrA);
+
+    printf("\n--- ADDRESSES ---\n");
+
+    printf("Address of a (&a): %p\n", (void *)&a);
+    printf("Address stored in ptrA: %p\n", (void *)ptrA);
+    printf("Address of ptrA (&ptrA): %p\n", (void *)&ptrA);
+
+    printf("\nAddress stored in ptrToPtrA: %p\n", (void *)ptrToPtrA);
+    printf("Address of ptrToPtrA (&ptrToPtrA): %p\n", (void *)&ptrToPtrA);
+
     return 0;
-    //pointer to function call
-}
-void square(int* n){
-    *n=(*n)*(*n);
-    printf("square = %d\n",*n);
 }
